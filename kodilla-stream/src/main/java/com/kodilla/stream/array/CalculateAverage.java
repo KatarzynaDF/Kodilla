70011c114374c95eda36eb05d1555c9505df929b
KatarzynaDF <dobiaskat@gmail.com>
2018-04-17 23:14:49 +0200

    7.5

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/CalculateAverage.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream.array;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public class CalculateAverage {
+
+    public OptionalDouble getAverage(Integer[] numbers1) {
+        Integer[] numbers2 = numbers1;
+        List<Integer> myNumbersList = Arrays.asList(numbers1);
+        IntStream myStream = IntStream.range(0, myNumbersList.size());
+        myStream.forEach(s -> System.out.println(s));
+        List<Integer> myNumbersList1 = Arrays.asList(numbers2);
+        IntStream myStream2 = IntStream.range(0, myNumbersList1.size());
+        OptionalDouble myAverage = myStream2.average();
+        return myAverage;
+    }
+}
