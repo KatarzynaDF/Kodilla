a09eb859baee150d43e1083d2d1a554adae857e4
KatarzynaDF <37115622+KatarzynaDF@users.noreply.github.com>
2018-08-15 17:37:27 +0200

    7.5a

--- kodilla-stream/src/main/java/com/kodilla/stream/array/CalculateAverage.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/CalculateAverage.java
@@ -7,12 +7,11 @@ import java.util.stream.IntStream;
 
 public class CalculateAverage {
 
-    public OptionalDouble getAverage(Integer[] numbers1) {
-        Integer[] numbers2 = numbers1;
+     public OptionalDouble getAverage(Integer[] numbers1) {
         List<Integer> myNumbersList = Arrays.asList(numbers1);
         IntStream myStream = IntStream.range(0, myNumbersList.size());
         myStream.forEach(s -> System.out.println(s));
-        List<Integer> myNumbersList1 = Arrays.asList(numbers2);
+        List<Integer> myNumbersList1 = Arrays.asList(numbers1);
         IntStream myStream2 = IntStream.range(0, myNumbersList1.size());
         OptionalDouble myAverage = myStream2.average();
         return myAverage;
